85dbcf2641ec84af56a13d86a50d43cdbf83d448
levelco <86658616+levelco@users.noreply.github.com>
2021-08-06 17:54:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -50,5 +50,5 @@ def handle_message(event):
      )
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
