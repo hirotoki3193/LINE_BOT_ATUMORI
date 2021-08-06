404297dc51cf4b7d701fc2c4e6fe9d47c5113371
levelco <86658616+levelco@users.noreply.github.com>
2021-08-06 17:03:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
 from flask import Flask, request, abort
-import os
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -11,44 +9,49 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+import os
 
 app = Flask(__name__)
 
-#環境変数取得
+#herokuの環境変数に設定された、LINE DevelopersのアクセストークンとChannelSecretを
+#取得するコード
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["LINE_BOT_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["LINE_BOT_SECRET"]
-
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+#herokuへのデプロイが成功したかどうかを確認するためのコード
 @app.route("/")
 def hello_world():
     return "hello world!"
 
+
+#LINE DevelopersのWebhookにURLを指定してWebhookからURLにイベントが送られるようにする
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # リクエストヘッダーから署名検証のための値を取得
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
+    # リクエストボディを取得
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    # 署名を検証し、問題なければhandleに定義されている関数を呼ぶ
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
+
+#以下でWebhookから送られてきたイベントをどのように処理するかを記述する
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
+# ポート番号の設定
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT"))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
